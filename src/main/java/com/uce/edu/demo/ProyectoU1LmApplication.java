d85d7d2cc48130a1a331f1941c4a6b15ffd45242
Fernando Mosquera <luismosquera97@gmail.com>
2022-06-14 20:03:30 -0500

    Taller 10: Ámbitos/Scope de Beans

--- src/main/java/com/uce/edu/demo/ProyectoU1LmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1LmApplication.java
@@ -1,20 +1,36 @@
 package com.uce.edu.demo;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.banco.service.IFachadaCuentaBancariaService;
+import com.uce.edu.demo.modelo.Estudiante;
+import com.uce.edu.demo.modelo.Materia;
+import com.uce.edu.demo.modelo.Matricula;
+import com.uce.edu.demo.modelo.ProfesorGeneral;
+import com.uce.edu.demo.modelo.ProfesorMateria;
+import com.uce.edu.demo.service.IMatriculaService;
 
 @SpringBootApplication
 public class ProyectoU1LmApplication implements CommandLineRunner {
 
 	@Autowired
-	
-	private IFachadaCuentaBancariaService bancaria;
+	private ProfesorGeneral profesorGeneral;
+
+	@Autowired
+	private ProfesorGeneral profesorGeneral1;
+
+	@Autowired
+	private ProfesorMateria materia;
+
+	@Autowired
+	private ProfesorMateria materia1;
+
+	@Autowired
+	private IMatriculaService iMatriculaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1LmApplication.class, args);
@@ -23,11 +39,35 @@ public class ProyectoU1LmApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		System.out.println("EJEMPLO SINGLETON");
+		this.profesorGeneral.setApellido("Guarnizo");
+		this.profesorGeneral.setNombre("Juan");
 
-		System.out.println("Cuenta Ahorro Futuro");
-		BigDecimal interes =this.bancaria.calcularInteres("1213");
-		System.out.println(interes);
-		
+		System.out.println(this.profesorGeneral);
+		System.out.println("------");
+
+		System.out.println(this.profesorGeneral1);
+		System.out.println("------");
+		this.profesorGeneral1.setNombre("Pepito");
+		System.out.println(this.profesorGeneral);
+
+		System.out.println("EJEMPLO PROTOTYPE");
+		this.materia.setNombre("Daniel");
+		this.materia.setApellido("Teran");
+
+		System.out.println(this.materia);
+
+		System.out.println("------");
+
+		System.out.println(this.materia1);
+
+		Matricula matriucla1 = new Matricula();
+		matriucla1.setEstudiante(new Estudiante());
+		matriucla1.setMateria(new ArrayList<Materia>());
+		matriucla1.setNumero("1234");
+
+		this.iMatriculaService.insertar(matriucla1);
+
 	}
 
 }
--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -4,37 +4,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.modelo.Matricula;
+import com.uce.edu.demo.modelo.ProfesorGeneral;
+import com.uce.edu.demo.modelo.ProfesorMateria;
 import com.uce.edu.demo.repository.IMatriculaRepository;
 
 @Service
 public class MatriculaServiceImpl implements IMatriculaService {
 
 	@Autowired
-	IMatriculaRepository matriculaRepository;
+	private IMatriculaRepository iMatriculaRepository;
+	
+	@Autowired
+	private ProfesorGeneral profesorGeneral;
+	
+	@Autowired
+	private ProfesorMateria profesorMateria;
 
 	@Override
 	public void insertar(Matricula m) {
 		// TODO Auto-generated method stub
-		this.matriculaRepository.insertar(m);
+		System.out.println("DI desde service SINGLETON "+ this.profesorGeneral);
+		System.out.println("DI desde service PROTOTYPE "+ this.profesorMateria);
+		
+		this.iMatriculaRepository.insertar(m);
 
 	}
 
 	@Override
 	public Matricula buscar(String numero) {
 		// TODO Auto-generated method stub
-		return this.matriculaRepository.buscar(numero);
+		return this.iMatriculaRepository.buscar(numero);
 	}
 
 	@Override
 	public void actualizar(Matricula m) {
 		// TODO Auto-generated method stub
-		this.matriculaRepository.actualizar(m);
+		this.iMatriculaRepository.actualizar(m);
 	}
 
 	@Override
 	public void borrar(String numero) {
 		// TODO Auto-generated method stub
-		this.matriculaRepository.borrar(numero);
+		this.iMatriculaRepository.borrar(numero);
 	}
 
 }
